77ce57978780d487612875e87b5c7e0652178dcb
weejan <weezan49@gmail.com>
2024-08-22 09:13:17 +0545

    Fixing style error which was breaking the code

--- src/components/IngredientList.jsx
+++ src/components/IngredientList.jsx
@@ -2,6 +2,6 @@ import Ingredients from "./Ingredients";
 
 export default function IngredientList({ food }) {
   return food.extendedIngredients.map((item) => {
-    return <Ingredients key={item.name} item={item} />;
+    return <Ingredients key={item.id} item={item} />;
   });
 }
--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -1,13 +1,13 @@
 import styles from "./ingredients.module.css";
 export default function Ingredients({ item }) {
   return (
-    <div key={item.name} style={styles.itemContainer}>
+    <div key={item.name}>
       <div className={styles.imgContainer}>
         <img
           src={`https://spoonacular.com/cdn/ingredients_100x100/` + item.image}
         ></img>
       </div>
-      <div className={styls.details}>
+      <div className={styles.details}>
         <h3>
           name:{item.nameClean}
           amount:{item.amount} {item.unit}
