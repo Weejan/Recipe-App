f5cbe5765b0459a2403f72ec7452925fd3949ac3
weejan <weezan49@gmail.com>
2024-08-22 13:02:03 +0545

    Fixing ingredient styling

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -1,7 +1,7 @@
 import styles from "./ingredients.module.css";
 export default function Ingredients({ item }) {
   return (
-    <div key={item.name}>
+    <div key={item.name} className={styles.itemContainer}>
       <div className={styles.imgContainer}>
         <img
           src={`https://spoonacular.com/cdn/ingredients_100x100/` + item.image}
@@ -10,6 +10,9 @@ export default function Ingredients({ item }) {
       <div className={styles.details}>
         <h3>
           name:{item.nameClean}
+          <br></br>
+        </h3>
+        <h3>
           amount:{item.amount} {item.unit}
         </h3>
       </div>
